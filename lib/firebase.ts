1c8c9392a37de6a666445396e10931e714931642
root <root@vmi2618243.contaboserver.net>
2025-06-09 06:12:59 +0200

    update fireabse

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID",
-}
+  apiKey: "AIzaSyCN6RqLYg2GEpLhgJ2y_pYgU-qFT-TKvLk",
+  authDomain: "superfan-a7e04.firebaseapp.com",
+  projectId: "superfan-a7e04",
+  storageBucket: "superfan-a7e04.firebasestorage.app",
+  messagingSenderId: "252277224014",
+  appId: "1:252277224014:web:8fad4e7cff1cd47b61c91f",
+  measurementId: "G-Z3KXDMWFZB"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
